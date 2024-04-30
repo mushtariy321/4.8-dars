9d492c8910d19d5e28f04e19c16e414a059f7d6c
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-04-30 20:22:07 +0300

    lesson-4.8

--- main.js
+++ main.js
@@ -7,8 +7,15 @@
 
 
 
-let result = (Math.min(4, 10, 888, 99, 100));
-let result2 = (Math.max(4, 10, 888, 99, 100));
-let number = [4, 10, 888, 99, 100]
-alert(number);
-alert(`Min number - ${result} \n Max number - ${result2}`);
+// let result = (Math.min(4, 10, 888, 99, 100));
+// let result2 = (Math.max(4, 10, 888, 99, 100));
+// let number = [4, 10, 888, 99, 100]
+// alert(number);
+// alert(`Min number - ${result} \n Max number - ${result2}`);
+
+
+
+let arr = [2, 4, 9, 10, 45, 67, 8, 90]
+var res = arr.filter((v, i, a) => v % 2 == 0)
+
+alert(`Berilgan son: ${arr}\n Juft sonlar: ${res}`)
