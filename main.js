b257c28d415cd3e5cd9ea6ea6e23c85521b4df15
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-04-30 20:08:45 +0300

    done'

---
+++ main.js
@@ -0,0 +1,14 @@
+// let arr = [8, 17., 1, 4, 10]
+// let num = +prompt(`son kiriting!`)
+// let res = arr.sort((v, i) => v - i)
+// console.log(res);
+// let result = res.map((arr) => arr * num)
+// console.log(result);
+
+
+
+let result = (Math.min(4, 10, 888, 99, 100));
+let result2 = (Math.max(4, 10, 888, 99, 100));
+let number = [4, 10, 888, 99, 100]
+alert(number);
+alert(`Min number - ${result} \n Max number - ${result2}`);
